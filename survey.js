86225493b476f40337c298936d051de8c73bf9a2
Iso <kagiso.k.mashigo@gmial.com>
2020-09-23 02:56:20 +0000

    simple bio generator app, working and unrefractored

--- survey.js
+++ survey.js
@@ -1,35 +1,40 @@
 const readline = require('readline');
-const answers = []
+const answers = {};
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-rl.question('What is your name? ', (answer) => {
-  console.log(`Great name: ${answer}`);
-  answers.push(`${answer}`);
-  rl.question('What is an activity you like doing? ', (answer) => {
-    console.log(`Sounds fun!: ${answer}`);
-    answers.push(`${answer}`);
+rl.question(`\nOh hey, what's your name? `, (answer) => {
+  console.log(`Great name, ${answer}.\n`);
+  answers["Name"] = answer;
+
+  rl.question(`What's your go-to hobby these days? `, (answer) => {
+    console.log(`Sounds like ${answer} is a vibe.\n`);
+    answers["Hobby"] = answer;
+
     rl.question('What do you listen to while doing that? ', (answer) => {
-      console.log(`Sets the mood huh? Nice: ${answer}`);
-      answers.push(`${answer}`);
-      rl.question('Which meal is your favourite? ', (answer) => {
-        console.log(`Good choice: ${answer}`);
-        answers.push(`${answer}`);
-        rl.question('What is your favourite thing to eat for that meal? ', (answer) => {
-          console.log(`Mmm tasty: ${answer}`);
-          answers.push(`${answer}`);
-          rl.question('Which sport is your absolute favourite? ', (answer) => {
-            console.log(`I mean, is there any better sport?: ${answer}`);
-            answers.push(`${answer}`);
-              rl.question('What is your superpower? In a few words, tell us what you are amazing at! ', (answer) => {
-                console.log(`Bruh, you're clearly gifted: ${answer}`);
-                answers.push(`${answer}`);
-                console.log(`Based on your sweet answers its safe to say that ${answers[0]} loves to get into the mood listening to ${answers[2]} while they eat ${answers[3]}. 
-As far as meals go nothing outshines ${answers[4]}, ever. Rumour has it that they use their superpower, ${answers[6]}, to 
-absolutely dominate at play ${answers[5]}. Pretty impressive stuff, ${answers[0]}.`)
+      console.log(`There's nothing like ${answer} to set the mood huh?\n`);
+      answers["Music"] = answer;
+
+      rl.question(`What's your favourite thing to eat for that meal? `, (answer) => {
+        console.log(`Mmm tasty ${answer}\n`);
+        answers["Food"] = answer;
+
+        rl.question(`What is your absolute favourite sport? `, (answer) => {
+           console.log(`I mean, is there any sport better than ${answer}?\n`);
+           answers["Sport"] = answer;
+
+           rl.question(`What's your superpower? Even the smallest thing. `, (answer) => {
+              console.log(`Bruh, ${answer}, you're clearly gifted. \n`);
+              answers["Superpower"] = answer;
+                //console.log(answers);
+              console.log(`Please wait while we generate your dope bio:\n`)
+
+  setTimeout(() => console.log(`Based on our sweet analysis its safe to say that ${answers["Name"]} loves to get into the mood listening to ${answers["Music"]} while they're ${answers["Hobby"]}. 
+As far as meals go nothing outshines ${answers["Food"]}, ever. Rumour has it that they use their superpower, ${answers["Superpower"]}, to 
+absolutely dominate at ${answers["Sport"]}. Pretty impressive stuff, ${answers["Name"]}.\n`), 3500)
 
                   rl.close();
                 });
@@ -38,7 +43,6 @@ absolutely dominate at play ${answers[5]}. Pretty impressive stuff, ${answers[0]
           });
         });
       });
-    });
 
 
 
